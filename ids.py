bf7da225f15b86097bd51be31a16c7896bf4c362
ACHRAF5258 <htmedachraf@gmail.com>
2026-06-08 01:10:27 +0200

    Update ids.py

--- ids.py
+++ ids.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 ╔══════════════════════════════════════════════════════╗
-║        PyIDS — Python Intrusion Detection System     ║
-║        Author: [Your Name]                           ║
-║        GitHub Portfolio Project                      ║
+║        IDS — Python Intrusion Detection System     ║
+║        Author: med achraf htiwech                    ║
+║                                                      ║
 ╚══════════════════════════════════════════════════════╝
 
 Detects:
